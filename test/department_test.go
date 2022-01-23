79548fa1765bffef83141bbda78d6927fd18e1b2
Ali <dev.aliakbar@gmail.com>
2022-01-23 12:42:08 +0530

    Department test started

--- test/department_test.go
+++ test/department_test.go
@@ -2,8 +2,36 @@ package test
 
 import (
 	"testing"
+
+	unitTest "github.com/Valiben/gin_unit_test"
+	"github.com/Valiben/gin_unit_test/utils"
+	"github.com/devaliakbar/gin_gorm_example/internals/features/department"
 )
 
-func TestDepartment(t *testing.T) {
-	t.Log("<<<Department Test>>>")
+func TestCreateDepartment(t *testing.T) {
+	input := department.CreateDepartmentInput{Name: "Testt"}
+
+	var res department.Department
+
+	err := unitTest.TestHandlerUnMarshalResp(utils.POST, "/department", "json", input, &res)
+
+	if err != nil {
+		t.Errorf("TestPostMappingClientNotFound: %v\n", err)
+		return
+	}
+
+	t.Log(res)
+}
+
+func TestGetAllDepartment(t *testing.T) {
+	var res []department.Department
+
+	err := unitTest.TestHandlerUnMarshalResp(utils.GET, "/departments", "json", nil, &res)
+
+	if err != nil {
+		t.Errorf("TestPostMappingClientNotFound: %v\n", err)
+		return
+	}
+
+	t.Log(res)
 }
